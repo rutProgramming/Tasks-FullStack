cbfafeafb4f4f4c8f4a46c1913bb2235c27768f7
rutprogramming <rutprogramming8978@gmail.com>
2025-02-05 22:12:02 +0200

    services

--- Node Express/server.js
+++ Node Express/server.js
@@ -1,6 +1,19 @@
-import renderApi from '@api/render-api';
+import axios from 'axios';
+import https from 'https';
+import dotenv from 'dotenv';
 
-renderApi.auth('rnd_ROFUUNY0ruebkK1DOYXWROYxFM9V');
-renderApi.listServices({includePreviews: 'true', limit: '20'})
-  .then(({ data }) => console.log(data))
-  .catch(err => console.error(err));
+dotenv.config();
+
+const API_KEY = process.env.RENDER_API_KEY;
+
+// יצירת סוכן מותאם אישית שמתעלם מבעיות אישור (פחות מסוכן מביטול מוחלט)
+const agent = new https.Agent({  
+  rejectUnauthorized: false 
+});
+
+axios.get('https://api.render.com/v1/services', {
+  headers: { Authorization: `Bearer ${API_KEY}` },
+  httpsAgent: agent  
+})
+.then(response => console.log(response.data))
+.catch(error => console.error(error));
